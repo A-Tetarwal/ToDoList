b2d7ca985fb4cc29f2a25785212005985c210011
A-Tetarwal <andashishwill@gmail.com>
2024-07-01 11:59:43 +0530

    commit2

---
+++ Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='font-bold text-center text-4xl p-2 border-b border-gray-700'>
+          Tasker &#9873;
+        </h1>
+    </div>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,11 +1,64 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import './page.css'
 
 const page = () => {
+  const [title, settitle] = useState('');
+  const [desc, setdesc] = useState('');
+  const [mainTask, setMainTask] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMainTask([...mainTask, { title, desc }])
+    settitle('');
+    setdesc('');
+    console.log(mainTask.title);
+  }
+
+
+  let renderTask = <h2>No Tasks Available</h2>
+  
+  renderTask =  mainTask.map((t, i) => {
+    return <div className="flex justify-between">
+      <h5>{t.title}</h5>
+      <h6>{t.desc}</h6>
+    </div>
+  })
   return (
     <div>
-      kya likhu
-    </div>
-  )
-}
+      <form onSubmit={submitHandler} className="text-black m-4 flex flex-col justify-center items-center">
+        <input 
+          type="text" 
+          className="p-2 placeholder:text-gray-500 text-sm border-b w-1  focus:outline-none focus:bg-slate-100" 
+          placeholder="Enter task here" 
+          required  
+          value={title}
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+        />
+        <textarea 
+          name="description" 
+          className="p-2 placeholder:text-gray-500 placeholder:text-sm border-b w-1 focus:outline-none focus:bg-slate-100" 
+          placeholder="Add Description"  
+          value={desc}
+          onChange={(e) =>{
+            setdesc(e.target.value);
+          }}
+        ></textarea>
+        <button 
+          className="text-white w-1 bg-red-600 hover:bg-red-700"
+        >Add</button>
+      </form>
 
-export default page
+      <br/>
+
+      <div className="p-8 bg-gray-950 ml-3 mr-3">
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default page;
